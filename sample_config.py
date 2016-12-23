0ae7fff01d1eefb43157b39c782b6ac652de51d5
EG <egun@antisigma.classics.utoronto.ca>
2016-12-23 11:57:59 -0500

    config-able defaults for which dbs are active

--- sample_config.py
+++ sample_config.py
@@ -9,6 +9,7 @@ DBNAME = 'hipparchiaDB'
 DBPASS = 'yourpasshere'
 LDICT = 'latin_dictionary'
 GDICT = 'greek_dictionary'
+
 # pick a number based on your cpu cores: on an 8 core machine diminishing returns kick in between 3 and 4 as the bottleneck shifts elsewhere
 # on a one-core virtual machine extra workers don't do much good and tend to just get in the way of one another: '1' seems to be best
 # a high number on a fast machine risks lockout from the db as too many requests come too fast: will need to recalibrate the default commit count if you go over 5
@@ -18,5 +19,8 @@ WORKERS = 3
 
 DEFAULTEARLIESTDATE = '-850'
 DEFAULTLATESTDATE = '1500'
-DEFAULTCORPUS = 'G'
 DEFAULTSORTORDER = 'shortname'
+DEFAULTGREEKCORPUSVALUE = 'yes'
+DEFAULTLATINCORPUSVALUE = 'no'
+DEFAULTINSCRIPTIONCORPUSVALUE = 'no'
+DEFAULTPAPYRUSCORPUSVALUE = 'no'
