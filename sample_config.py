765326c4fa4cd26cf1f829bd045d1b331dbf5227
EG <egun@antisigma.classics.utoronto.ca>
2017-01-23 14:12:44 -0500

    more documentation for the options in config.py

--- sample_config.py
+++ sample_config.py
@@ -1,22 +1,36 @@
+# Flask variables; DEBUG=True is considered to be a serious security hazard in a networked environment
 DEBUG=False
 SECRET_KEY = 'yourkeyhereitshouldbelongandlooklikecryptographicgobbledygook'
 
-# a read-only db user is highly recommended; write access means inviting a world of hurt
-DBUSER = 'hippa_rd'
-DBHOST = '127.0.0.1'
-DBPORT = 5432
-DBNAME = 'hipparchiaDB'
-DBPASS = 'yourpassheretrytomakeitstrongplease'
-LDICT = 'latin_dictionary'
-GDICT = 'greek_dictionary'
 
+# Hipparchia debug variables: show DB locations of hits and/or the raw HTML markup inside the DB
+# there are no security implications here
+DBDEBUGMODE = 'no'
+HTMLDEBUGMODE = 'no'
+
+
+# Hipparchia performance variable
 # pick a number based on your cpu cores: on an 8 core machine diminishing returns kick in between 3 and 4 as the bottleneck shifts elsewhere
 # on a one-core virtual machine extra workers don't do much good and tend to just get in the way of one another: '1' seems to be best
 # a high number on a fast machine risks lockout from the db as too many requests come too fast: will need to recalibrate the default commit count if you go over 5
 # your mileage will indeed vary, but N > cores*(.5) is probably not going to do much good.
 WORKERS = 3
 
-# the default settings for various values
+
+# DB variables
+# a read-only db user is highly recommended; write access means inviting a world of hurt
+DBUSER = 'hippa_rd'
+DBHOST = '127.0.0.1'
+DBPORT = 5432
+DBNAME = 'hipparchiaDB'
+DBPASS = 'yourpassheretrytomakeitstrongplease'
+
+LDICT = 'latin_dictionary'
+GDICT = 'greek_dictionary'
+
+# the default settings for various values: what you get if you clear the session
+# these can all be set to different values via the web interface
+
 DEFAULTEARLIESTDATE = '-850'
 DEFAULTLATESTDATE = '1500'
 # valid options are: shortname, authgenre, location, provenance, universalid, converted_date
@@ -37,6 +51,3 @@ DEFAULTLATINCORPUSVALUE = 'no'
 DEFAULTINSCRIPTIONCORPUSVALUE = 'no'
 DEFAULTPAPYRUSCORPUSVALUE = 'no'
 DEFAULTCHRISTIANCORPUSVALUE = 'no'
-
-DBDEBUGMODE = 'no'
-HTMLDEBUGMODE = 'no'
