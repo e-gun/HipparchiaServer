39242245193cb39ac2c968b8776412bc3478c127
EG <egun@antisigma.classics.utoronto.ca>
2017-06-03 12:44:23 -0400

    delete views.py [and hope for the best]

--- server/__init__.py
+++ server/__init__.py
@@ -4,6 +4,5 @@ from flask import Flask
 hipparchia = Flask(__name__)
 hipparchia.config.from_object('config')
 
-# from server import views
-from server.startup import *
+from server import startup
 from server.routes import *
