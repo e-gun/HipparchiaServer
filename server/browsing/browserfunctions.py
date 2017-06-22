a879ea49909338e37eefb1edd1883c08f3895dae
EG <egun@antisigma.classics.utoronto.ca>
2017-06-21 21:30:57 -0400

    more fussing with insertparserids(): less clunky code

--- server/browsing/browserfunctions.py
+++ server/browsing/browserfunctions.py
@@ -244,38 +244,42 @@ def insertparserids(lineobject):
 
 	theline = re.sub(r'(\<.*?\>)',r'*snip*\1*snip*',theline)
 	hyphenated = lineobject.hyphenated
-	segments = theline.split('*snip*')
+	segments = [s for s in theline.split('*snip*') if s]
 	newline = ''
 
+	# it is tricky to get the tags and the spaces right: it looks like you either get one kind of glitch or another kind
+	# the lesser of two evils has been chosen?
+	spacedclosings = re.compile(r'[,;.?!:·′“”†]$')
+	sometimesspacedclosings = re.compile(r'[)⟩}]$')
+	openings = re.compile(r'^[(‵„“⟨{†]')
+
 	for seg in segments:
-		try:
-			if seg[0] == '<':
-				# this is markup don't 'observe' it
-				newline += seg
-			else:
-				words = seg.split(' ')
-				lastword = words[-1]
-				for word in words:
-					try:
-						if word[-1] in [',', ';', '.', '?', '!', ')', '′', '“', '·', '⟩', '}']:
-							try:
-								if word[-6:] != '&nbsp;':
-									word = '<observed id="{wa}">{wa}</observed>{wb}'.format(wa=word[:-1], wb=word[-1])
-							except:
-								word = '<observed id="{wa}">{wa}</observed>{wb}'.format(wa=word[:-1], wb=word[-1])
-						elif word[-1] == '-' and word == lastword:
-							word = '<observed id="{h}">{w}</observed> '.format(h=hyphenated, w=word)
-						elif word[0] in ['(', '‵', '„', '\'', '⟨', '{']:
-							word = '{wa}<observed id="{wb}">{wb}"</observed>'.format(wa=word[0], wb=word[1:])
-						else:
-							word = '<observed id="{w}">{w}</observed> '.format(w=word)
-						newline += word
-					except:
-						# word = ''
-						pass
-		except:
-			# seg = ''
-			pass
+		if seg[0] == '<':
+			# this is markup don't 'observe' it
+			newline += seg
+		else:
+			words = seg.split(' ')
+			lastword = words[-1]
+			for word in words:
+				try:
+					if re.search(spacedclosings, word) or re.search(sometimesspacedclosings, word):
+						try:
+							if word[-6:] != '&nbsp;':
+								word = '<observed id="{wa}">{wa}</observed>{wb} '.format(wa=word[:-1], wb=word[-1])
+						except:
+							word = '<observed id="{wa}">{wa}</observed>{wb} '.format(wa=word[:-1], wb=word[-1])
+					# elif re.search(sometimesspacedclosings, word):
+					# 	word = '<observed id="{wa}">{wa}</observed>{wb}'.format(wa=word[:-1], wb=word[-1])
+					elif word[-1] == '-' and word == lastword:
+						word = '<observed id="{h}">{w}</observed> '.format(h=hyphenated, w=word)
+					elif re.search(openings, word):
+						word = '{wa}<observed id="{wb}">{wb}</observed> '.format(wa=word[0], wb=word[1:])
+					else:
+						word = '<observed id="{w}">{w}</observed> '.format(w=word)
+					newline += word
+				except:
+					# word = ''
+					pass
 
 	# address dodgy word division and ill-formatted word spacing issues: 'ἱε[ ρέω ]ϲ' instead of 'ἱε[ρέω]ϲ'
 	spacerightbracket = re.compile(r'</observed> </span><observed id="(.*?)">](.*?)</observed>')
