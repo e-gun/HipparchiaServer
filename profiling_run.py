50a939f2ab69157cb7a817f5cd45eaaece5edf23
EG <egun@antisigma.classics.utoronto.ca>
2018-01-27 19:22:31 -0500

    perl -pi -w -e 's/Copyright: E Gunderson 2016-17/Copyright: E Gunderson 2016-18/g;' *.py

--- profiling_run.py
+++ profiling_run.py
@@ -1,7 +1,7 @@
 #!../bin/python
 """
 	HipparchiaServer: an interface to a database of Greek and Latin texts
-	Copyright: E Gunderson 2016-17
+	Copyright: E Gunderson 2016-18
 	License: GNU GENERAL PUBLIC LICENSE 3
 		(see LICENSE in the top level directory of the distribution)
 """
--- server/static/indexandtextmaker.js
+++ server/static/indexandtextmaker.js
@@ -1,6 +1,6 @@
 //
 //	HipparchiaServer: an interface to a database of Greek and Latin texts
-//	Copyright: E Gunderson 2016-17
+//	Copyright: E Gunderson 2016-18
 //	License: License: GNU GENERAL PUBLIC LICENSE 3
 //      (see LICENSE in the top level directory of the distribution)
 
