242b087c7abd3f1692e1faa0b4c9ea1d12e7ba28
EG <egun@antisigma.classics.utoronto.ca>
2018-09-10 09:17:00 -0400

    tablenamer() was unused (right?)

--- server/dbsupport/tablefunctions.py
+++ server/dbsupport/tablefunctions.py
@@ -6,42 +6,7 @@
 		(see LICENSE in the top level directory of the distribution)
 """
 
-import random
-
 from uuid import uuid4
-
-from server.hipparchiaobjects.dbtextobjects import dbAuthor
-
-
-def tablenamer(authorobject: dbAuthor, thework: int) -> str:
-	"""
-
-	tell me the name of your table
-	work 1 is stored as 0: try not to create a table 0; lots of unexpected results can stem from this off-by-one slip
-
-	:param authorobject:
-	:param thework:
-	:return:
-	"""
-
-	wk = authorobject.listofworks[thework - 1]
-	# wk = authorobject.listworks()[thework - 1]
-	nm = authorobject.authornumber
-	wn = wk.worknumber
-
-	lg = wk.language
-	# how many bilingual authors are there again?
-	if lg == 'G':
-		pr = 'gr'
-	elif lg == 'L':
-		pr = 'lt'
-	else:
-		pr = ''
-		print('oh, I do not speak {lg} and I will be unable to access a DB'.format(lg=lg))
-
-	workdbname = '{p}{n}w{w}'.format(p=pr, n=nm, w=wn)
-
-	return workdbname
 
 
 def assignuniquename() -> str:
