235ae63b998b497c59f803c7a1af78e0c19a3a81
e-gun <egun@antisigma.classics.utoronto.ca>
2019-09-26 07:12:09 -0400

    fedora and ubuntu hate setdefaultisolation(), etc.; pass on the exception

--- server/hipparchiaobjects/connectionobject.py
+++ server/hipparchiaobjects/connectionobject.py
@@ -101,7 +101,12 @@ class GenericConnectionObject(object):
 		return getattr(self.dbconnection, 'autocommit')
 
 	def setdefaultisolation(self):
-		self.dbconnection.set_isolation_level(psycopg2.extensions.ISOLATION_LEVEL_DEFAULT)
+		try:
+			self.dbconnection.set_isolation_level(psycopg2.extensions.ISOLATION_LEVEL_DEFAULT)
+		except psycopg2.InterfaceError:
+			# ubuntu19 and fedora31 coughed this up
+			# psycopg2.InterfaceError: connection already closed
+			pass
 
 	def cursor(self):
 		return self.curs
@@ -272,7 +277,9 @@ class PooledConnectionObject(GenericConnectionObject):
 		try:
 			self.dbconnection.set_session(readonly=False)
 		except psycopg2.OperationalError:
-			consolewarning('change your connection type to "simple" in "networksettings.py"; pooled connections are failing', color='red')
+			# ubuntu19 and fedora31 constantly send you here
+			# consolewarning('change your connection type to "simple" in "networksettings.py"; pooled connections are failing', color='red')
+			pass
 		self.setdefaultisolation()
 		self.pool.putconn(self.dbconnection, key=self.uniquename)
 		# print('connection returned to pool:', self.uniquename)
