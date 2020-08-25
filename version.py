ceb581fa86038193a4a9f604d98897a678d973a4
e-gun <egun@antisigma.classics.utoronto.ca>
2020-08-25 11:01:32 -0400

    need a new release because of the morphology bug

--- version.py
+++ version.py
@@ -10,10 +10,10 @@ from sys import argv
 from os import path
 
 
-stable = False
-release = False
+stable = True
+release = True
 
-hipparchiaserverversion = '1.6.6'
+hipparchiaserverversion = '1.6.7'
 plus = '+'
 supplement = '[DEVEL]'
 
