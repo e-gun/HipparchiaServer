d66bed7b50428455821a87599b0b215b29141c4a
e-gun <egun@antisigma.classics.utoronto.ca>
2021-03-27 15:03:55 -0400

    1.6.12 release precipitated by gensim 4.0.0 breakage

--- version.py
+++ version.py
@@ -10,10 +10,10 @@ from sys import argv
 from os import path
 
 
-stable = False
-release = False
+stable = True
+release = True
 
-hipparchiaserverversion = '1.6.11'
+hipparchiaserverversion = '1.6.12'
 plus = '+'
 supplement = '[DEVEL]'
 
