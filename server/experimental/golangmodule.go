6ce61c1ee86312b16a1c6c4d7eb86962422447c9
e-gun <egun@antisigma.classics.utoronto.ca>
2021-04-22 13:24:19 -0400

    gosearch module can now interact with python thanks to gopy

--- server/experimental/golangmodule.go
+++ server/experimental/golangmodule.go
@@ -1,4 +1,5 @@
-package main
+package hipparchiagolangsearching
+// package main
 
 import (
 	"C"
@@ -11,30 +12,6 @@ import (
 	"runtime"
 	"sync"
 )
-
-/*
-
-for python cli
-
-import redis
-import json
-rc = redis.ConnectionPool(host='127.0.0.1', port=6379, db=0)
-c = redis.Redis(connection_pool=rc)
-l = {'TempTable': '', 'PsqlQuery': 'SELECT wkuniversalid, index, level_05_value, level_04_value, level_03_value, level_02_value, level_01_value, level_00_value, marked_up_line, accented_line, stripped_line, hyphenated_words, annotations FROM lt0914 WHERE  ( stripped_line ~* $1 )  LIMIT 200', 'PsqlData': '(^|\\s)strage(\\s|$)'}
-c.sadd('queries', json.dumps(l))
-
-*/
-
-// https://tutorialedge.net/golang/go-redis-tutorial/
-// https://golangdocs.com/golang-postgresql-example
-// https://gobyexample.com/waitgroups
-// https://www.ardanlabs.com/blog/2020/07/extending-python-with-go.html
-// https://hackernoon.com/extending-python-3-in-go-78f3a69552ac
-// https://blog.filippo.io/building-python-modules-with-go-1-5/
-
-// https://github.com/go-python/gopy
-// pip install pybindgen
-
 
 const (
 	rp			= `{"Addr": "localhost:6379", "Password": "", "DB": 0}`
@@ -83,32 +60,52 @@ func main() {
 
 	var k string
 	var c int64
-	var t int
+	var g int
 	var r string
 	var p string
 
 	flag.StringVar(&k, "k", "queries", "redis key to use")
 	flag.Int64Var(&c, "c", 200, "max hit count")
-	flag.IntVar(&t, "t", 5, "number of goroutines to dispatch")
+	flag.IntVar(&g, "t", 5, "number of goroutines to dispatch")
 	flag.StringVar(&r, "r", rp, "redis logon information (as a JSON string)")
 	flag.StringVar(&p, "p", psq, "psql logon information (as a JSON string)")
 	flag.Parse()
 
-	o := gosearch(k, c, t, []byte(r), []byte(p))
+	rl := decoderedislogin([]byte(r))
+	po := decodepsqllogin([]byte(p))
+
+	o := HipparchiaGolangSearcher(k, c, g, rl, po)
 	fmt.Println("results sent to redis as", o)
 }
 
+//Generate new redis credentials
+func NewRedisLogin(ad string, pw string, db int) *RedisLogin {
+	return &RedisLogin{
+		Addr: ad,
+		Password: pw,
+		DB: db,
+	}
+}
+
+
+//Generate new postgres credentials
+func NewPostgresLogin(ho string, po int, us string, pw string, db string) *PostgresLogin {
+	return &PostgresLogin{
+		Host: ho,
+		Port: po,
+		User: us,
+		Pass: pw,
+		DBName: db,
+	}
+}
 
 //Execute a series of SQL queries stored in redis by dispatching a collection of goroutines
-func gosearch(searchkey string, hitcap int64, threadcount int, redislogininfo []byte, psqllogininfo []byte) string {
-	runtime.GOMAXPROCS(threadcount)
-
-	r := decoderedislogin(redislogininfo)
-	p := decodepsqllogin(psqllogininfo)
+func HipparchiaGolangSearcher(searchkey string, hitcap int64, goroutines int, r RedisLogin, p PostgresLogin) string {
+	runtime.GOMAXPROCS(goroutines)
 
 	var awaiting sync.WaitGroup
 
-	for i:=0; i < threadcount; i++ {
+	for i:=0; i < goroutines; i++ {
 		awaiting.Add(1)
 		go grabber(i, hitcap, searchkey, r, p, &awaiting)
 	}
@@ -123,7 +120,7 @@ func gosearch(searchkey string, hitcap int64, threadcount int, redislogininfo []
 func decoderedislogin(redislogininfo []byte) RedisLogin {
 	var rl RedisLogin
 	err := json.Unmarshal(redislogininfo, &rl)
-	CheckError(err)
+	checkerror(err)
 	return rl
 }
 
@@ -131,7 +128,7 @@ func decoderedislogin(redislogininfo []byte) RedisLogin {
 func decodepsqllogin(psqllogininfo []byte) PostgresLogin {
 	var ps PostgresLogin
 	err := json.Unmarshal(psqllogininfo, &ps)
-	CheckError(err)
+	checkerror(err)
 	return ps
 }
 
@@ -143,17 +140,17 @@ func grabber(clientnumber int, hitcap int64, searchkey string, r RedisLogin, p P
 	redisclient := redis.NewClient(&redis.Options{Addr: r.Addr, Password: r.Password, DB: r.DB})
 	_, err := redisclient.Ping().Result()
 	defer redisclient.Close()
-	CheckError(err)
+	checkerror(err)
 	// fmt.Println("Connected to redis")
 
 	psqlconn := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable",
 		p.Host, p.Port, p.User, p.Pass, p.DBName)
 	psqlcursor, err := sql.Open("postgres", psqlconn)
-	CheckError(err)
+	checkerror(err)
 
 	defer psqlcursor.Close()
 	err = psqlcursor.Ping()
-	CheckError(err)
+	checkerror(err)
 	// fmt.Println(fmt.Sprintf("grabber #%d Connected to %s on PostgreSQL", clientnumber, dbname))
 
 	resultkey := searchkey + "_results"
@@ -167,18 +164,18 @@ func grabber(clientnumber int, hitcap int64, searchkey string, r RedisLogin, p P
 		// [b] decode it
 		var prq PrerolledQuery
 		err = json.Unmarshal([]byte(byteArray), &prq)
-		CheckError(err)
+		checkerror(err)
 		// fmt.Printf("%+v\n", prq)
 
 		// [c] build a temp table if needed
 		if prq.PsqlQuery != "" {
 			_, err := psqlcursor.Query(prq.TempTable)
-			CheckError(err)
+			checkerror(err)
 		}
 
 		// [d] execute the query
 		foundrows, err := psqlcursor.Query(prq.PsqlQuery, prq.PsqlData)
-		CheckError(err)
+		checkerror(err)
 
 		// [e] iterate through the finds
 		defer foundrows.Close()
@@ -188,18 +185,18 @@ func grabber(clientnumber int, hitcap int64, searchkey string, r RedisLogin, p P
 			err = foundrows.Scan(&thehit.WkUID, &thehit.TbIndex, &thehit.Lvl5Value, &thehit.Lvl4Value, &thehit.Lvl3Value,
 				&thehit.Lvl2Value, &thehit.Lvl1Value, &thehit.Lvl0Value, &thehit.MarkedUp, &thehit.Accented,
 				&thehit.Stripped, &thehit.Hypenated, &thehit.Annotations)
-			CheckError(err)
+			checkerror(err)
 			//fmt.Println(thehit)
 
-			// [e2] if you have not hit the find cap, store the result in 'querykey_results'
+			// [e2] if you have not hit the cap on finds, store the result in 'querykey_results'
 			hitcount, err := redisclient.SCard(resultkey).Result()
-			CheckError(err)
+			checkerror(err)
 			if hitcount >= hitcap {
 				// trigger the break in the outer loop
 				redisclient.Del(searchkey)
 			} else {
 				jsonhit, err := json.Marshal(thehit)
-				CheckError(err)
+				checkerror(err)
 				redisclient.SAdd(resultkey, jsonhit)
 				// fmt.Println(fmt.Sprintf("grabber #%d added a result to %s", clientnumber, resultkey))
 			}
@@ -208,7 +205,7 @@ func grabber(clientnumber int, hitcap int64, searchkey string, r RedisLogin, p P
 }
 
 
-func CheckError(err error) {
+func checkerror(err error) {
 	if err != nil {
 		panic(err)
 	}
